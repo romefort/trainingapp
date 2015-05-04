7b9dfbdd2f46ab94639d2b337d9f8befb6ec1555
Johann Romefort <romefort@gmail.com>
2015-05-04 14:57:00 +0200

    modify greeting

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Buro2.0' });
+  res.render('index', { title: 'Willkommen im Buro2.0' });
 });
 
 module.exports = router;
