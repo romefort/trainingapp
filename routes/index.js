479bc672dcc454b3c4ed2c35d43a3d3f91c8ef2e
Johann Romefort <romefort@gmail.com>
2015-05-05 09:47:09 +0200

    update for compose

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var redis = require("redis"),
+        client = redis.createClient(process.env.REDIS_PORT_6379_TCP_PORT, process.env.REDIS_PORT_6379_TCP_ADDR, null);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hey Willkommen im Buro2.0' });
+   client.set("foo_rand000000000000", "xOK!!!");
+
+   // This will return a JavaScript String
+   client.get("foo_rand000000000000", function (err, reply) {
+       console.log(reply.toString()); // Will print `OK`
+   });
+
+  res.render('index', { title: 'Hello Hey Willkommen im Buro2.0' });
 });
 
 module.exports = router;
